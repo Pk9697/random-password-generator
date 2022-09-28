dbc5955973e38b89558544621571e4519a6af198
Prashant Kumar <prashant.aman9@gmail.com>
2022-09-28 16:58:03 +0530

    add the functionality to toggle if user wants to include numbers or symbols in pass or not

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // console.log("Script loaded")
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+const allCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
 "S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
 "p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";",
 "<",">",".","?",
 "/"];
+const numbersCharacters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbolCharacters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";",
+"<",">",".","?",
+"/"];
+const onlyCharacters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z"];
 
 //fetch btn and h3's
 const btn=document.getElementById("btn");
@@ -14,14 +25,16 @@ const pass2=document.getElementById("pass2");
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 
-function randomCharactersArray(passLength=15){
+const checkbox1=document.getElementById("checkbox1");
+const checkbox2=document.getElementById("checkbox2");
+function randomCharactersArray(passLength=15,charactersArr=allCharacters){
     //of length 15
     // const charArray=[];
     let randomPassword="";
     for(let i=0;i<passLength;i++){
-        const randomCharIdx=Math.floor(Math.random()*characters.length);
+        const randomCharIdx=Math.floor(Math.random()*charactersArr.length);
         // charArray.push(characters[randomCharIdx]);
-        randomPassword+=characters[randomCharIdx];
+        randomPassword+=charactersArr[randomCharIdx];
         
     }
     // console.log(charArray);
@@ -30,11 +43,19 @@ function randomCharactersArray(passLength=15){
 // console.log(randomCharactersArray())
 
 btn.addEventListener("click",function(){
-    console.log(slider.value)
-    pass1.textContent=randomCharactersArray(slider.value);
-    pass2.textContent=randomCharactersArray(slider.value);
-    // console.log("clicked")
-
+    if(checkbox1.checked && checkbox2.checked){
+        pass1.textContent=randomCharactersArray(slider.value,allCharacters);
+        pass2.textContent=randomCharactersArray(slider.value,allCharacters);
+    }else if(checkbox1.checked){
+        pass1.textContent=randomCharactersArray(slider.value,numbersCharacters);
+        pass2.textContent=randomCharactersArray(slider.value,numbersCharacters);
+    }else if(checkbox2.checked){
+        pass1.textContent=randomCharactersArray(slider.value,symbolCharacters);
+        pass2.textContent=randomCharactersArray(slider.value,symbolCharacters);
+    }else{
+        pass1.textContent=randomCharactersArray(slider.value,onlyCharacters);
+        pass2.textContent=randomCharactersArray(slider.value,onlyCharacters);
+    }
 
 })
 
